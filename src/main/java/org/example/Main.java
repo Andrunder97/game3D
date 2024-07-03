87416ed3be3627c9eeb2c6aeb02b54b0840cd73c
Andrey <anbes1997@gmail.com>
2024-07-03 23:30:49 +0400

    add: 3d model of cube

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.joml.Matrix4f;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
@@ -16,6 +17,11 @@ import static org.lwjgl.system.MemoryUtil.*;
 public class Main {
 
     private long window; //id window
+
+    private float posX = 0.0f;
+    private float posY = 0.0f;
+    private float posZ = -5.0f;
+    private float angle = 0.0f;
 
     public void run() {
         init();
@@ -44,14 +50,26 @@ public class Main {
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        window = glfwCreateWindow(800, 600, "3D game!", NULL, NULL);
         if (window == NULL)
             throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
+            if (action == GLFW_PRESS || action == GLFW_REPEAT) { //move object
+                switch (key) {
+                    case GLFW_KEY_UP -> posY += 0.05f;
+                    case GLFW_KEY_DOWN -> posY -= 0.05f;
+                    case GLFW_KEY_LEFT -> posX -= 0.05f;
+                    case GLFW_KEY_RIGHT -> posX += 0.05f;
+                    case GLFW_KEY_W -> posZ += 0.05f;
+                    case GLFW_KEY_S -> posZ -= 0.05f;
+                }
+            }
+
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(window, true);
+            }
         });
 
         // Get the thread stack and push a new frame
@@ -86,12 +104,75 @@ public class Main {
         //create area
         GL.createCapabilities();
 
+        glEnable(GL_DEPTH_TEST); //set depth for 3D
+        Matrix4f projection = new Matrix4f().perspective((float) Math.toRadians(45.0), 800.0f / 600.0f, 0.1f, 100f); //set projection
+        FloatBuffer buf = BufferUtils.createFloatBuffer(16);
+        projection.get(buf);
+
         //Set the color to area
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
         //drawing
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            //set 3D projection
+            glMatrixMode(GL_PROJECTION);
+            glLoadMatrixf(buf);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            glTranslatef(posX, posY, posZ); //object coords
+            glRotatef(angle, 0f, 1f, 0f); //rotation on Y
+            glScalef(0.33f, 0.33f, 0.33f); //object size
+
+            //object description
+            glBegin(GL_QUADS);
+
+            //front
+            glColor3f(1, 0, 0);
+            glVertex3f(-0.5f, -0.5f, 0.5f);
+            glVertex3f(0.5f, -0.5f, 0.5f);
+            glVertex3f(0.5f, 0.5f, 0.5f);
+            glVertex3f(-0.5f, 0.5f, 0.5f);
+
+            //back
+            glColor3f(0, 1, 0);
+            glVertex3f(-0.5f, -0.5f, -0.5f);
+            glVertex3f(-0.5f, 0.5f, -0.5f);
+            glVertex3f(0.5f, 0.5f, -0.5f);
+            glVertex3f(0.5f, -0.5f, -0.5f);
+
+            //top
+            glColor3f(0, 0, 1);
+            glVertex3f(-0.5f, 0.5f, -0.5f);
+            glVertex3f(-0.5f, 0.5f, 0.5f);
+            glVertex3f(0.5f, 0.5f, 0.5f);
+            glVertex3f(0.5f, 0.5f, -0.5f);
+
+            //bottom
+            glColor3f(1, 1, 0);
+            glVertex3f(-0.5f, -0.5f, -0.5f);
+            glVertex3f(0.5f, -0.5f, -0.5f);
+            glVertex3f(0.5f, -0.5f, 0.5f);
+            glVertex3f(-0.5f, -0.5f, 0.5f);
+
+            //right
+            glColor3f(1, 0, 1);
+            glVertex3f(0.5f, -0.5f, -0.5f);
+            glVertex3f(0.5f, 0.5f, -0.5f);
+            glVertex3f(0.5f, 0.5f, 0.5f);
+            glVertex3f(0.5f, -0.5f, 0.5f);
+
+            //left
+            glColor3f(0, 1, 1);
+            glVertex3f(-0.5f, -0.5f, -0.5f);
+            glVertex3f(-0.5f, -0.5f, 0.5f);
+            glVertex3f(-0.5f, 0.5f, 0.5f);
+            glVertex3f(-0.5f, 0.5f, -0.5f);
+
+            glEnd();
+
+            angle += 0.5f;
 
             glfwSwapBuffers(window); // swap the color buffers
 
